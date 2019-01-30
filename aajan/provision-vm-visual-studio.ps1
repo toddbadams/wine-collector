f9b946003b009e109514084395c1300c0e7df6e5
Todd B. Adams <toddbadams@msn.com>
2019-01-30 06:19:33 +0000

    updates

--- aajan/provision-vm-visual-studio.ps1
+++ aajan/provision-vm-visual-studio.ps1
@@ -17,46 +17,56 @@ $version = "latest"
 $cred = Get-Credential -Message "Enter a username and password for the virtual machine."
 
 #Create or check for existing resource group
-$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
-if(!$resourceGroup)
-{
-    Write-Host "Creating resource group '$resourceGroupName' in location '$location'";
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
-}
-else {
-    Write-Host "Using existing resource group '$resourceGroupName'";
-}
+New-AzureRmResourceGroup `
+  -Name $resourceGroupName `
+  -Location $location
 
 # Create a subnet configuration
-$subnetConfig = Get-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnetName -ErrorAction SilentlyContinue
-if(!$subnetConfig) {
-    Write-Host "Creating subnet '$subnetName'";
-    $subnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix 192.168.1.0/24
-}
-else {
-    Write-Host "Using existing Subnet '$subnetName'";
-}
+$subnetConfig = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name $subnetName `
+  -AddressPrefix 192.168.1.0/24
 
 # Create a virtual network
-$vnet = New-AzureRmVirtualNetwork -resourceGroup $resourceGroupName -Location $location `
-  -Name $vnetName -AddressPrefix 192.168.0.0/16 -Subnet $subnetConfig
+$vnet = New-AzureRmVirtualNetwork `
+  -resourceGroup $resourceGroupName `
+  -Location $location `
+  -Name $vnetName `
+  -AddressPrefix 192.168.0.0/16 `
+  -Subnet $subnetConfig
 
 # Create a public IP address and specify a DNS name
-$pip = New-AzureRmPublicIpAddress -resourceGroup $resourceGroupName -Location $location `
-  -Name "mypublicdns$(Get-Random)" -AllocationMethod Static -IdleTimeoutInMinutes 4
+$pip = New-AzureRmPublicIpAddress `
+  -resourceGroup $resourceGroupName `
+  -Location $location `
+  -Name "mypublicdns$(Get-Random)" `
+  -AllocationMethod Static -IdleTimeoutInMinutes 4
 
 # Create an inbound network security group rule for port 3389
-$nsgRuleRDP = New-AzureRmNetworkSecurityRuleConfig -Name $nsgRuleName  -Protocol Tcp `
-  -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * `
+$nsgRuleRDP = New-AzureRmNetworkSecurityRuleConfig `
+  -Name $nsgRuleName  `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 1000 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
   -DestinationPortRange 3389 -Access Allow
 
 # Create a network security group
-$nsg = New-AzureRmNetworkSecurityGroup -resourceGroup $resourceGroupName -Location $location `
-  -Name $nsgName -SecurityRules $nsgRuleRDP
+$nsg = New-AzureRmNetworkSecurityGroup `
+  -resourceGroup $resourceGroupName `
+  -Location $location `
+  -Name $nsgName  `
+  -SecurityRules $nsgRuleRDP
 
 # Create a virtual network card and associate with public IP address and NSG
-$nic = New-AzureRmNetworkInterface -Name $nicName -resourceGroup $resourceGroupName -Location $location `
-  -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+$nic = New-AzureRmNetworkInterface `
+  -Name $nicName `
+  -resourceGroup $resourceGroupName `
+  -Location $location `
+  -SubnetId $vnet.Subnets[0].Id `
+  -PublicIpAddressId $pip.Id `
+  -NetworkSecurityGroupId $nsg.Id
 
 # Create a virtual machine configuration
 $vmConfig = New-AzureRmVMConfig -VMName $vmName -VMSize Standard_D2_v2 | `
